dd81ba61059e5846a4ec5e99c4f076124f8ee363
JosephHonpah <honpahj@gmail.com>
2025-01-05 21:12:49 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-northeast-1_WPjvHSJpD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3a33f3lhn201qcpkdqj90tm711', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-northeast-1' // e.g. us-east-2
+        userPoolId: "ap-northeast-1_Ll8WkVDNP",
+        userPoolClientId: "56d38r6h6cv0n8o67qpgk5qtgj",
+        region: 'ap-northeast-1',
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '',
     }
 };
