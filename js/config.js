21f3c89f25385f8b3276e37b4cf3af239cc00812
JosephHonpah <honpahj@gmail.com>
2025-01-09 11:42:51 +0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_DUcHadqt5',
-        userPoolClientId: '1b9pva5go6tivi20f27suaabe3',
+        userPoolId: 'us-west-2_Nw7NsTC94',
+        userPoolClientId: '6i2oc136ttnduoi7uah12uda86',
         region: 'us-west-2',
     },
     api: {
