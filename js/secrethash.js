0b1c906224442648e1becc82285c60cfe011c505
JosephHonpah <honpahj@gmail.com>
2025-01-09 11:54:00 +0400

    Update secrethash.js

--- js/secrethash.js
+++ js/secrethash.js
@@ -1,1 +1,8 @@
+const crypto = require('crypto');
+
+function generateSecretHash(username, clientId, clientSecret) {
+    const message = username + clientId;
+    const key = clientSecret;
+    return crypto.createHmac('SHA256', key).update(message).digest('base64');
+}
  
